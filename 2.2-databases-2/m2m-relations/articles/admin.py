d2a81b96ef73e10f165630d856bccd6fbc3e76e0
Roman Mamin <roman74mamin@gmail.com>
2022-09-05 23:32:24 +0300

    0509

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        flag = 0
+        for form in self.forms:
+
+            if form.cleaned_data.get('DELETE'):
+                continue
+
+            if form.cleaned_data.get('is_main'):
+                flag += 1
+
+            if flag > 1:
+                raise ValidationError('Выберите 1 основной раздел.')
+
+        if flag == 0:
+            raise ValidationError('Выберите 1 основной раздел.')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+@admin.register(Tag)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ['name']
